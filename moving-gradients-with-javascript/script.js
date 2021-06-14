d851741fc0924f45069b2f83729f7149fbe896ab
unknown <rjkaz@comcast.net>
2021-06-14 14:45:49 -0800

    4 min mark

--- moving-gradients-with-javascript/script.js
+++ moving-gradients-with-javascript/script.js
@@ -12,6 +12,41 @@ class App {
         this.canvas = document.createElement('canvas')
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
+
+        this.totalParticles = 1;
+        this.particles = [];
+        this.maxRadius = 90;
+        this.minRadius = 40;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.heiht = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.createParticles(); 
+
+    }
+
+    createParticles() {
+        let curColor = 0;
+        this.particles = [];
+
+        for (let i = 0; i < this.totalParticles; i++ ){
+
+        }
+    }
+    animate() {
+
     }
 }
 
@@ -19,4 +54,4 @@ window.onload = () => {
     new App();
 }
 
-// left off at 1:31 from video https://www.youtube.com/watch?v=D6EiRSRhsbQ
+// left off at 4:08 from video https://www.youtube.com/watch?v=D6EiRSRhsbQ
